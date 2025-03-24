1aa9575140aadbf2d48103d8564cd025aafcbc1d
Wang <645565009@qq.com>
2025-03-24 16:11:45 +0800

    add ai email assistant

--- src/main/java/com/ginkgooai/core/ai/BeCoreAiApplication.java
+++ src/main/java/com/ginkgooai/core/ai/BeCoreAiApplication.java
@@ -2,8 +2,10 @@ package com.ginkgooai.core.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BeCoreAiApplication {
 
     public static void main(String[] args) {
